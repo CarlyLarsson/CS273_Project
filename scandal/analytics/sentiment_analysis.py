7f08459179d3db8ccca66fe207f23d199eb3ab8c
CarlyLarsson <carly@umail.ucsb.edu>
2019-05-08 22:06:13 -0700

    added totaling and top 20

--- scandal/analytics/sentiment_analysis.py
+++ scandal/analytics/sentiment_analysis.py
@@ -3,6 +3,8 @@ import csv
 from glob import glob
 import sys
 import os
+import numpy as np
+import pandas as pd
 
 def sent_analyze(tweet):
     lexicon = Empath()
@@ -27,4 +29,23 @@ def get_tweets():
                     row.append(result)
                 tsvwriter.writerow(row)
 
-get_tweets()
+def totaling():
+    #Go through all the given tweets empath results and return the top 10 category scoreres
+    for file in glob(str(sys.argv[1])+"/*empath.tsv"):
+        with open(file, 'rU') as f:
+            tsvreader = csv.reader(f, delimiter='\t') 
+            tsvreader = csv.reader(f, delimiter='\t') 
+            fields = next(tsvreader)[20:]
+            #194 categories
+            totaling = np.zeros(194)
+
+            for row in tsvreader:
+                rates = np.zeros(194)
+                count = 0
+                for s in row[20:]:
+                    rates[count] = float(s)
+                    count += 1
+                totaling = totaling + rates
+
+            track = pd.Series(totaling, fields)
+            return track.sort_values(ascending=False)[:20]
